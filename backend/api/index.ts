d8f647359f86c261dae35117845e61ed9a1d15b1
Manas Saxena <manassaxena160601@gmail.com>
2025-09-28 00:15:31 +0530

    remove vercel.json

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,3 +1,3 @@
 import serverless from 'serverless-http'
-import app from '../src/app'
+import app from '../src/app'   // make sure app.ts exports `export default app`
 export default serverless(app)
